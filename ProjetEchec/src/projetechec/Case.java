dc133870a528b91a3f86c902c2961eabf089709e
Florian <florian.faccin@hotmail.fr>
2018-01-17 12:18:09 +0100

    Premiers pas dans le jeu !

--- ProjetEchec/src/projetechec/Case.java
+++ ProjetEchec/src/projetechec/Case.java
@@ -5,10 +5,32 @@
  */
 package projetechec;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 /**
  *
  * @author jholmieres
  */
-public class Case {
+public class Case  extends MouseAdapter{
     
+    private int i;
+    private int j;
+    private PlateauFrame plateau;
+
+    public Case(int i, int j, PlateauFrame unPlateau){
+        this.i = i;
+        this.j = j;
+        this.plateau = unPlateau;
+    }
+    
+    public void mouseClicked(MouseEvent e){
+        if(plateau.getControler().getJoueurEnCours() == 0){
+            if(plateau.getControler().getJoueur0().aPiece(i, j)){
+                System.out.println(i + " " + j);
+                plateau.getControler().getJoueur0().jouer(i, j, 5, 5);
+                plateau.updateAffichageAll();
+            }
+        }
+    }
 }
