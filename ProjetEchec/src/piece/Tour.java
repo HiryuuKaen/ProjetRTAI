1d066ce290055490801d56ea090047d69d6baaa5
Florian <florian.faccin@hotmail.fr>
2018-01-17 12:39:26 +0100

    on commit comme il faut cette fois !

---
+++ ProjetEchec/src/piece/Tour.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piece;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author jholmieres
+ */
+public class Tour extends Piece {
+    
+    public Tour(int positionX, int positionY, int joueur) {
+        super(positionX, positionY);
+        
+        if(joueur == 1) 
+            setIcone(new ImageIcon("./src/icons/tourN.png"));
+        else
+            setIcone(new ImageIcon("./src/icons/tourB.png"));
+    }
+    
+}
